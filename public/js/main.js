93e7c81c15dce0e984f7100fcc6de28d92c725a1
manu183 <manuellang183@gmail.com>
2018-04-06 16:40:41 +0200

    add entries model

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
-$(document).ready(() => {
+alert("123");
 
-  // Place JavaScript code here...
+$(document).ready(() => {
+  alert("123");
+  $("#addEntry").on("click", function() {
+    $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
+    $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+  });
 
 });
